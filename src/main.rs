501451efe75a45c84e62d460e30f384ed0488efb
Carl Hagred <carl.hagred@gmail.com>
2026-06-14 18:53:15 +0200

    feat: initial switch to clap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use clap::Parser;
 use std::fs;
 
 mod models;
@@ -12,47 +12,43 @@ use parser::parse_requests;
 mod engine;
 use engine::send_request;
 
+#[derive(Parser, Debug)]
+#[command(name = "CrabCall")]
+#[command(about = "A fast HTTP client and TUI", long_about = None)]
+struct Cli {
+    /// The .http file to execute (Optional, if left blank, interactive mode will be launched)
+    file: Option<String>,
+
+    /// The specific request index to run (Optional)
+    index: Option<usize>,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: crabcall <file.http>");
-        return Ok(());
-    }
-    let raw_text = fs::read_to_string(&args[1])?;
+    let cli = Cli::parse();
+    if let Some(filepath) = cli.file {
+        let raw_text = fs::read_to_string(&filepath)?;
 
-    let my_tokens = tokenize(raw_text.trim());
-    println!("{:#?}", my_tokens);
+        let my_tokens = tokenize(raw_text.trim());
+        let parsed_request = parse_requests(my_tokens);
 
-    let parsed_request = parse_requests(my_tokens);
-    println!("Parsed request:\n{:?}", parsed_request);
-
-    if args.len() >= 3 {
-        let index_arg = &args[2];
-        match index_arg.parse::<usize>() {
-            Ok(idx) => {
-                if idx > 0 && idx <= parsed_request.len() {
-                    let target_req = &parsed_request[idx - 1];
-                    send_request(target_req);
-                } else {
-                    println!(
-                        "Error: Request index {} is out of bounds. This file has {} requests.",
-                        idx,
-                        parsed_request.len(),
-                    );
-                }
-            }
-            Err(_) => {
+        if let Some(idx) = cli.index {
+            if idx > 0 && idx <= parsed_request.len() {
+                let target_req = &parsed_request[idx - 1];
+                send_request(target_req);
+            } else {
                 println!(
-                    "Error: '{}' is not a valid number. Please provide a request index like '1' or '2'.",
-                    index_arg
+                    "Error: Request index {} is out of bounds. This file has {} requests.",
+                    idx,
+                    parsed_request.len()
                 );
+            }
+        } else {
+            for req in &parsed_request {
+                send_request(req);
             }
         }
     } else {
-        for req in &parsed_request {
-            send_request(req);
-        }
+        println!("Coming soon...");
     }
-
     Ok(())
 }
